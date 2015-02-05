ab9957df171ed25d3ee229ac7ef25c7ce5bb78d8
Jim Sparkman <jim.sparkman@houselogix.com>
2015-02-05 18:54:07 -0500

    Refactor

--- routing_demo/riotcontrol.js
+++ routing_demo/riotcontrol.js
@@ -1,17 +1,15 @@
+var _RiotControlApi = ['on','one','off','trigger']
 var RiotControl = {
   _stores: [],
   addStore: function(store) {
     this._stores.push(store)
-  },
-  trigger: function() {
-    var args = [].slice.call(arguments)
-    this._stores.forEach(function(el,i,a){
-      el.trigger.apply(null, args)
-    })
-  },
-  on: function(ev, callback) {
-    this._stores.forEach(function(el,i,a){
-      el.on(ev, callback)
-    })
   }
 }
+_RiotControlApi.forEach(function(api){
+	RiotControl[api] = function() {
+		var args = [].slice.call(arguments)
+		this._stores.forEach(function(el){
+	      el[api].apply(null, args)
+	    })
+	}
+})
