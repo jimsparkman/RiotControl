3e8e6d5faba1eadf07697709af6300b0aed7311a
Jim Sparkman <jim.sparkman@houselogix.com>
2015-02-05 18:53:04 -0500

    Refactor

--- demo/riotcontrol.js
+++ demo/riotcontrol.js
@@ -1,30 +1,15 @@
+var _RiotControlApi = ['on','one','off','trigger']
 var RiotControl = {
   _stores: [],
   addStore: function(store) {
     this._stores.push(store)
-  },
-  trigger: function() {
-    var args = [].slice.call(arguments)
-    this._stores.forEach(function(el){
-      el.trigger.apply(null, args)
-    })
-  },
-  on: function(ev, cb) {
-    this._stores.forEach(function(el){
-      el.on(ev, cb)
-    })
-  },
-  off: function(ev, cb) {
-    this._stores.forEach(function(el){
-      if (cb)
-        el.off(ev, cb)
-      else
-        el.off(ev)        
-    })    
-  },
-  one: function(ev, cb) {
-    this._stores.forEach(function(el){
-      el.one(ev, cb)
-    })
   }
 }
+_RiotControlApi.forEach(function(api){
+	RiotControl[api] = function() {
+		var args = [].slice.call(arguments)
+		this._stores.forEach(function(el){
+	      el[api].apply(null, args)
+	    })
+	}
+})
