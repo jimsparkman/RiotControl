7198b2b88541eae1b64440ecaceff4ffd80fd52f
Christopher Fredén <c.freden@gmail.com>
2015-02-09 14:16:53 +0100

    Added CommonJS module support

--- riotcontrol.js
+++ riotcontrol.js
@@ -1,15 +1,17 @@
-var _RiotControlApi = ['on','one','off','trigger']
 var RiotControl = {
   _stores: [],
   addStore: function(store) {
-    this._stores.push(store)
+    this._stores.push(store);
   }
-}
-_RiotControlApi.forEach(function(api){
-	RiotControl[api] = function() {
-		var args = [].slice.call(arguments)
-		this._stores.forEach(function(el){
-	      el[api].apply(null, args)
-	    })
-	}
-})
+};
+
+['on','one','off','trigger'].forEach(function(api){
+  RiotControl[api] = function() {
+    var args = [].slice.call(arguments);
+    this._stores.forEach(function(el){
+      el[api].apply(null, args);
+    });
+  };
+});
+
+if (typeof(module) !== 'undefined') module.exports = RiotControl;
